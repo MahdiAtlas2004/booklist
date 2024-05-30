1ffb63908352b4a5679d4194742999a4fc61b019
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-30 20:09:02 +0430

    BookForm added

---
+++ src/components/BookForm.js
@@ -0,0 +1,36 @@
+import React, { useState, useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
+
+const BookForm = () => {
+  const { addBook } = useContext(BookContext);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook(title, author);
+    setTitle("");
+    setAuthor("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        placeholder="book title"
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type="text"
+        value={author}
+        placeholder="author"
+        onChange={(e) => setAuthor(e.target.value)}
+        required
+      />
+      <input type="submit" value="add book" />
+    </form>
+  );
+};
+
+export default BookForm;
