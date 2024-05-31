409eca2e88e289e47750f91a68d3367f265a875f
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-31 16:01:22 +0430

    localStorage added to project for saving the data in user's browser.

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,13 +1,19 @@
-import React, { useReducer, createContext } from "react";
+import React, { useReducer, createContext, useEffect } from "react";
 import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const loacalData = localStorage.getItem("books");
+    return loacalData ? JSON.parse(loacalData) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return (
-    <BookContext.Provider value={{ books, dispatch}}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
