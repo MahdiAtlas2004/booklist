5c06d30c632f6531018b97e41a5a07be21e2f5c1
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-06-04 06:30:34 +0430

    confirmation alert added for delete process

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -4,8 +4,14 @@ import { BookContext } from "../contexts/BookContext";
 //This one represent evey single book with author name.
 const BookDetails = ({ book }) => {
   const { dispatch } = useContext(BookContext);
+  const handleDelete = () => {
+    const res = window.confirm("The book will be deleted.");
+    if (res) {
+      dispatch({ type: 'REMOVE_BOOK', id: book.id });
+    }
+  };
   return (
-    <li onClick={() => dispatch({type: 'REMOVE_BOOK', id: book.id})}>
+    <li onClick={handleDelete}>
       <div className="title">{book.title}</div>
       <div className="author">{book.author}</div>
     </li>
